1a75a452ec9f9d86cb7fafb1cd9a4a8f359a75bd
MithunPrabhu777 <prabhuofficial7@gmail.com>
2021-08-17 23:18:03 +0530

    localStoage Added

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './ListItems.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import FlipMove from 'react-flip-move';
 
 function ListItems(props){
+
+    const [lists,setLists] = useState([]);
+    
     const items = props.items;
+
+    useEffect(() => {
+        setLists(getLocalItems());
+        console.log(items);
+    },[]);
+
+    useEffect(() => {
+        localStorage.setItem('lists',JSON.stringify(items))
+        console.log(items);
+    },[items]);
+
+    const getLocalItems = () => {
+        const list = localStorage.getItem('lists')
+        console.log(list);
+        if(list){
+            return JSON.parse(localStorage.getItem('lists'))
+        }
+        else{
+            return [];
+        }
+    }
+    console.log(localStorage);
+    
     const listItems = items.map(item =>
    {
        return <div className="list" key={item.key}>
